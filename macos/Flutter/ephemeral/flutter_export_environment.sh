c83b17d9ba4529e27bbaec624275bbac7ff7b034
Shegun MONTCHO <shekmontcho@gmail.com>
2025-10-27 07:52:09 +0100

    1.0.176151+1761518419

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\DELL\Documents\IR\UserApp"
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.22.2/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/shegunmontcho/StudioProjects/opensi/User_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=4"
+export "FLUTTER_BUILD_NAME=1.0.176151"
+export "FLUTTER_BUILD_NUMBER=1761518419"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
